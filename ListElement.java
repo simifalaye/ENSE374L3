ae92c18ad63aa30886f7e0715bbe8539983ba9a3
simifalaye <simifalaye1@gmail.com>
2015-10-26 23:44:21 -0600

    Phase 1: Completed

--- ListElement.java
+++ ListElement.java
@@ -6,22 +6,38 @@
  import java.util.*;
   
  public class ListElement {
-	 private ListElement next;
-	 private ListElement previous;
+	 private ListElement next; // = new ListElement();
+	 private ListElement previous; // = new ListElement();
 	 private int data;
 	 
 	 public ListElement()
 	 {
-		 this.data = 0;
-		 this.next = null;
-		 this.previous = null;
+		this.data = 0;
+		this.next = null;
+		this.previous = null;
 	 }
 	 
 	 public void setData(int data){
-		 this.data = data;
+		this.data = data;
 	 }
 	 
 	 public int getData(){
-		 return this.data;
+		return this.data;
+	 }
+	 
+	 public void setNext(ListElement next){
+		this.next = next;
+	 }
+	 
+	 public ListElement getNext(){
+		return this.next;
+	 }
+	 
+	 public void setPrev(ListElement prev){
+		this.previous = prev;
+	 }
+	 
+	 public ListElement getPrev(){
+		return this.previous;
 	 }
  }
